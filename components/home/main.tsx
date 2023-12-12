ff98c8637dc0bb2fbb9e38b79e5a39ccb33c4b12
Mantvydas Ciuklys <m.ciuklys@gethiper.co>
2023-12-12 13:16:37 +0200

    Fixes bug

--- components/home/main.tsx
+++ components/home/main.tsx
@@ -5,9 +5,6 @@ export default function Main() {
   return (
     <>
     <div className="my-4 grid w-full h-full max-h-screen-xl max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
-      <Card
-        demo={<Map></Map>}
-      ></Card>
     </div>
   </>
   );
